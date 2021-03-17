9fc96fb95c67cbdeafc9ca33d6a5c5178f14f475
Ivanna Zobniv <ivannazobniv333@gmail.com>
2021-03-17 17:53:45 +0200

    Rebase main template to base.html. In home.html the Articles were added. Home views was rebuild for Articles

--- mygis/views.py
+++ mygis/views.py
@@ -2,6 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from mygis.models import Articles
+
+
 def home(request):
     #return HttpResponse("Hello world")
-    return render(request, 'mygis/home.html')
+    #return render(request, 'mygis/home.html')
+    articles = Articles.objects.all()
+    context = {
+        'articles': articles
+    }
+    return render(request, 'mygis/home.html', context)
